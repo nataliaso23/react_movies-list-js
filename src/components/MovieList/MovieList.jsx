cdb23067e52990ba1a67b39694fea283c6ba9c05
Natalia <natalia.23santos@hotmail.com>
2025-03-23 21:28:06 -0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,10 @@
 import './MovieList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MovieList = ({ movies }) => {
-  return (
-    <div className="movies">
-      {movies.map(movie => (
-        <MovieCard key={movie.imdbId} movie={movie} />
-      ))}
-    </div>
-  );
-};
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
+);
